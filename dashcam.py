b63fb0ff7e458dada4aac4cdaa78f3d204cc01fd
anvinder <anvinder_singh26@yahoo.co.in>
2022-01-11 22:59:45 -0800

    Update dashcam.py

--- dashcam.py
+++ dashcam.py
@@ -1,8 +1,27 @@
+import shutil
 import cv2
 import time
+import os
 cv2.namedWindow("dashcam")
 video = cv2.VideoCapture(1)
 last_save_time = -10
+now = time.time()
+path1 = "C:\\Users\\qwerty\\PycharmProjects\\generic\\dashcam"
+os.chdir(path1)
+usage = shutil.disk_usage(path1)
+free_space = usage[1]
+
+bytes = float(free_space)
+kb = bytes / 1024
+Mb = kb/(1024*1024)
+print("size in Mb = ", Mb)
+
+for filename in os.listdir(path1):
+    if os.path.getmtime(os.path.join(path1, filename)) < now - 7*86400:
+        if os.path.isfile(os.path.join(path1, filename)):
+            print(filename)
+            os.remove(os.path.join(path1, filename))
+
 while video.isOpened():
     rval, frame = video.read()
     if not rval:
